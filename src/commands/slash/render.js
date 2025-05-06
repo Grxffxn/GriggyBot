8c2cfe682cb797d0c05c6131ce34bfff4747e823
Grxffxn <38299311+Grxffxn@users.noreply.github.com>
2025-05-05 22:55:37 -0400

    one-liners, move msgs to top (lang file coming soon)

--- src/commands/slash/render.js
+++ src/commands/slash/render.js
@@ -2,66 +2,75 @@ const { SlashCommandBuilder, MessageFlags } = require('discord.js');
 const axios = require('axios');
 const { getConfig } = require('../../utils/configUtils');
 
-const defaultSkinType = 'wide';
+const MESSAGES = {
+    BEDROCK_NOT_SUPPORTED: 'Sorry, Bedrock renders are not supported.',
+    PLAYER_NOT_FOUND: 'That player doesn\'t exist :(',
+    FETCH_ERROR: 'An error occurred while fetching player data.',
+    INVALID_RENDER_TYPE: 'Invalid render type.',
+};
+
+const renderPaths = {
+    body: 'full/384',
+    head: 'head/256',
+    avatar: 'face/256',
+    bust: 'bust/256',
+};
 
 module.exports = {
-	data : new SlashCommandBuilder()
-		.setName('render')
-		.setDescription('Player renders')
-		.addStringOption(option =>
-			option.setName('render_type')
-				.setDescription('Type of render')
-				.setRequired(true)
-				.addChoices(
-					{ name: 'Full Body', value: 'body' },
-					{ name: '3D Skull', value: 'head' },
-					{ name: '2D Avatar', value: 'avatar' },
-					{ name: '3D Bust', value: 'bust' },
-				),
-		)
-		.addStringOption(option =>
-			option.setName('username')
-				.setDescription('Minecraft username')
-				.setRequired(true),
-		)
-		.addStringOption(option =>
-			option.setName('skintype')
-				.setDescription('Skin type')
-				.setRequired(false)
-				.addChoices(
-					{ name: 'Steve', value: 'wide' },
-					{ name: 'Alex', value: 'slim' },
-				),
-		),
-	async run(interaction) {
-		const username = interaction.options.getString('username');
-		const rendertype = interaction.options.getString('render_type');
-		const skintype = interaction.options.getString('skintype') || defaultSkinType;
+    data: new SlashCommandBuilder()
+        .setName('render')
+        .setDescription('Player renders')
+        .addStringOption(option =>
+            option.setName('render_type')
+                .setDescription('Type of render')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'Full Body', value: 'body' },
+                    { name: '3D Skull', value: 'head' },
+                    { name: '2D Avatar', value: 'avatar' },
+                    { name: '3D Bust', value: 'bust' },
+                ),
+        )
+        .addStringOption(option =>
+            option.setName('username')
+                .setDescription('Minecraft username')
+                .setRequired(true),
+        )
+        .addStringOption(option =>
+            option.setName('skintype')
+                .setDescription('Skin type')
+                .setRequired(false)
+                .addChoices(
+                    { name: 'Steve', value: 'wide' },
+                    { name: 'Alex', value: 'slim' },
+                ),
+        ),
+    async run(interaction) {
+        const username = interaction.options.getString('username');
+        const rendertype = interaction.options.getString('render_type');
+        const skintype = interaction.options.getString('skintype') || 'wide';
 
-		if (username.startsWith('.')) {
-			return interaction.reply({ content: 'Sorry, Bedrock renders are not supported.', flags: MessageFlags.Ephemeral });
-		}
-		try {
-			const response = await axios.get(`https://api.mojang.com/users/profiles/minecraft/${username}`);
+        if (username.startsWith('.')) return interaction.reply({ content: MESSAGES.BEDROCK_NOT_SUPPORTED, flags: MessageFlags.Ephemeral });
 
-			if (!response.data) {
-				return interaction.reply({content: 'That player doesn\'t exist :(', flags: MessageFlags.Ephemeral });
-			}
+        const renderPath = renderPaths[rendertype];
 
-			const playerUUID = response.data.id;
-			const trimmedUUID = playerUUID.replace(/-/g, '');
+        try {
+            const response = await axios.get(`https://api.mojang.com/users/profiles/minecraft/${username}`);
 
-			const config = getConfig();
-			let renderUrl = '';
-			if (rendertype === 'body') renderUrl = `${config.baseRenderUrl}full/384/${trimmedUUID}?${skintype}`;
-			if (rendertype === 'head') renderUrl = `${config.baseRenderUrl}head/256/${trimmedUUID}?${skintype}`;
-			if (rendertype === 'avatar') renderUrl = `${config.baseRenderUrl}face/256/${trimmedUUID}?${skintype}`;
-			if (rendertype === 'bust') renderUrl = `${config.baseRenderUrl}bust/256/${trimmedUUID}?${skintype}`;
+            if (!response.data) return interaction.reply({ content: MESSAGES.PLAYER_NOT_FOUND, flags: MessageFlags.Ephemeral });
 
-			interaction.reply({ content: renderUrl });
-		} catch (err) {
-			interaction.client.log('Error fetching player data:', 'ERROR', err);
-			interaction.reply({ content: 'An error occurred while fetching player data.', flags: MessageFlags.Ephemeral });
-		}
-	},
+            const playerUUID = response.data.id.replace(/-/g, '');
+            const config = getConfig();
+
+            const renderUrl = `${config.baseRenderUrl}${renderPath}/${playerUUID}?${skintype}`;
+
+            await interaction.reply({ content: renderUrl })
+                .catch(err => {
+                    interaction.client.log('Error sending render URL:', 'ERROR', err);
+                });
+        } catch (err) {
+            interaction.client.log('Error fetching player data:', 'ERROR', err);
+            return interaction.reply({ content: MESSAGES.FETCH_ERROR, flags: MessageFlags.Ephemeral });
+        }
+    },
 };
